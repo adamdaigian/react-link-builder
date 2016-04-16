94c17219f986ad0af3f90511cf1e0d69a41dfffd
Adam <adam.daigian@gmail.com>
2016-04-15 17:15:13 -0700

    pushed to s3

--- assets/tracker.js
+++ assets/tracker.js
@@ -1,6 +1,15 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+console.log('ran')
+copy_button = document.getElementById('copy-button');
+function copyText () {
+  console.log('clicked')
+  var copy_text = $('#link').val().toString();
+  console.log('clicked')
+  if (copy_text !== undefined) {
+    console.log('made it hurr')
+    clipboardData.setData(copy_text)
+    console.log('cool brah')
+  } else {
+    console.log('failed')
+  }
+}
+copy_button.onClick = copyText();
